a118a554466f606a40f7f395d20c25443f8c4ae3
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-04 11:52:32 +0200

    remove chart, introduce useSharedState

---
+++ react/hooks/index.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { push } from "../client/hosts";
+import useWebSocket from "react-use-websocket";
+
+function useSharedState(initValue: any, channelId: string) {
+  const [data, setData] = useState(initValue);
+  const wsUrl = `wss://${push}?channelId=${channelId}&apiKey=&appId=demo`;
+  const { sendJsonMessage } = useWebSocket(wsUrl, {
+    share: true,
+    shouldReconnect: () => true,
+    onMessage: (event) => {
+      const data = event?.data ? JSON.parse(event?.data) : null;
+      if (data) {
+        setData(data);
+      }
+    },
+    onClose: (event) => {},
+    onError: (event) => {},
+  });
+
+  const update = (newData: any) => {
+    sendJsonMessage(newData);
+  };
+
+  return [data, update];
+}
+export { useSharedState };
