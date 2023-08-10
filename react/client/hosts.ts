d0e35469caedfedfb40d753436c3b61a8292bb86
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-10 10:47:12 +0200

    fix local

--- react/client/hosts.ts
+++ react/client/hosts.ts
@@ -1,16 +1,7 @@
-const push =
-  process.env.NODE_ENV === "development"
-    ? "localhost:8080"
-    : "push.getshippr.com";
+const push = "push.getshippr.com";
 
-const pushHttp =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8080"
-    : "http://push.getshippr.com";
+const pushHttp = "http://push.getshippr.com";
 
-const pushWs =
-  process.env.NODE_ENV === "development"
-    ? "ws://localhost:8080"
-    : "wss://push.getshippr.com";
+const pushWs = "wss://push.getshippr.com";
 
 export { push, pushHttp, pushWs };
