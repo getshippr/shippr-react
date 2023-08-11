26aec73ad8ade9f6bc32666bde25c2c7b461e5a4
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-11 16:41:35 +0200

    upgrade react client

--- react/client/hosts.ts
+++ react/client/hosts.ts
@@ -1,13 +1,7 @@
-const push =
-  process.env.NODE_ENV === "development"
-    ? "localhost:8080"
-    : "push.getshippr.com";
+const push = "push.getshippr.com";
 
-const pushHttp = "http://push.getshippr.com";
+const pushHttp = "https://push.getshippr.com";
 
-const pushWs =
-  process.env.NODE_ENV === "development"
-    ? "ws://localhost:8080"
-    : "wss://push.getshippr.com";
+const pushWs = "wss://push.getshippr.com";
 
 export { push, pushHttp, pushWs };
