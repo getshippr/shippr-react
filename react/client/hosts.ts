724dd18530d8b3aaa05cc2848147cfb8fff8832d
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-15 20:43:11 +0200

    fix

--- react/client/hosts.ts
+++ react/client/hosts.ts
@@ -2,6 +2,8 @@ const push = "push.getshippr.com";
 
 const pushHttp = "https://push.getshippr.com";
 
-const pushWs = "wss://push.getshippr.com";
+const pushWs = `${
+  process.env.NODE_ENV === "development" ? "ws" : "wss"
+}://push.getshippr.com`;
 
 export { push, pushHttp, pushWs };
