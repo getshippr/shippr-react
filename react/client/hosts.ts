5ec577f8dd2ae76062ac29b12d4f862b92a03cad
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-15 20:48:33 +0200

    test

--- react/client/hosts.ts
+++ react/client/hosts.ts
@@ -3,7 +3,7 @@ const push = "push.getshippr.com";
 const pushHttp = "https://push.getshippr.com";
 
 const pushWs = `${
-  process.env.NODE_ENV === "development" ? "ws" : "wss"
+  process.env.NEXT_PUBLIC_NODE_ENV === "development" ? "ws" : "wss"
 }://push.getshippr.com`;
 
 export { push, pushHttp, pushWs };
