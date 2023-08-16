c8390e9a94efaee569ffb03a0f34b434c6cd8032
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-16 10:12:48 +0200

    fix js build

--- react/client/hosts.ts
+++ react/client/hosts.ts
@@ -2,12 +2,6 @@ const push = "push.getshippr.com";
 
 const pushHttp = "https://push.getshippr.com";
 
-const isLocalhost =
-  process.env.NODE_ENV === "development" ||
-  (typeof "window" !== "undefined"
-    ? /localhost/.test(window.location.hostname)
-    : false);
-
-const pushWs = `${isLocalhost ? "ws" : "wss"}://push.getshippr.com`;
+const pushWs = `wss://push.getshippr.com`;
 
 export { push, pushHttp, pushWs };
