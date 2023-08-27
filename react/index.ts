8d5e3d20d1b2a638b9abf5128e476df64cd1d6ae
Bap. Coquelle <coquellebaptiste@gmail.com>
2023-08-27 16:55:35 +0200

    forgotten exports

--- react/index.ts
+++ react/index.ts
@@ -2,7 +2,21 @@ import { init } from "./client";
 import ChannelPresence from "./components/ChannelPresence";
 import Message from "./components/Message";
 import MultiCursors from "./components/MultiCursors";
+import ShipprProvider, {
+  usePresence,
+  useSharedState,
+  useShipprConfig,
+} from "./components/ShipprProvider";
 
-export { init, ChannelPresence, Message, MultiCursors };
+export {
+  init,
+  ChannelPresence,
+  Message,
+  MultiCursors,
+  ShipprProvider,
+  usePresence,
+  useSharedState,
+  useShipprConfig,
+};
 export * from "./types";
 export default init;
